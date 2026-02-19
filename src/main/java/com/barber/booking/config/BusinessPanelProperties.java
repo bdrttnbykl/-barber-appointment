3d57edbea754a762dc3cef7bfd33b0ec587adcb3
bedrettin_7 <thebybed@gmail.com>
2026-02-19 15:16:49 +0300

    Update BusinessPanelProperties.java

--- src/main/java/com/barber/booking/config/BusinessPanelProperties.java
+++ src/main/java/com/barber/booking/config/BusinessPanelProperties.java
@@ -3,35 +3,50 @@ package com.barber.booking.config;
 import java.time.Duration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.util.StringUtils;
+
+import jakarta.annotation.PostConstruct;
 
 @ConfigurationProperties(prefix = "business.panel")
 public class BusinessPanelProperties {
 
-	private String username = "";
-	private String password = "";
-	private Duration sessionTtl = Duration.ofHours(8);
+    // Default boş bırakma: config'ten gelecek
+    private String username;
+    private String password;
+    private Duration sessionTtl = Duration.ofHours(8);
 
-	public String getUsername() {
-		return username;
-	}
+    @PostConstruct
+    void validate() {
+        if (!StringUtils.hasText(username)) {
+            throw new IllegalStateException("Missing config: business.panel.username (or env BUSINESS_PANEL_USERNAME)");
+        }
+        if (!StringUtils.hasText(password)) {
+            throw new IllegalStateException("Missing config: business.panel.password (or env BUSINESS_PANEL_PASSWORD)");
+        }
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public Duration getSessionTtl() {
-		return sessionTtl;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public void setSessionTtl(Duration sessionTtl) {
-		this.sessionTtl = sessionTtl;
-	}
+    public Duration getSessionTtl() {
+        return sessionTtl;
+    }
+
+    public void setSessionTtl(Duration sessionTtl) {
+        this.sessionTtl = sessionTtl;
+    }
 }
+
